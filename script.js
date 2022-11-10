fce4d4ead993bb9a1700022ad4b7615f2e27bf57
Tony.Chen <tony.chen@hy-tech.com.tw>
2022-11-10 17:50:14 +0800

    11/10

---
+++ script.js
@@ -0,0 +1,18 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element , then play
+async function selectMediaStream(){
+    try {
+        
+    } catch (error) {
+        //Catch Error Here
+        console.log('error here:',error);
+    }
+}
+
+
+//On Load
+selectMediaStream();
+
+
